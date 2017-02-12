ab1e17b0cd500b5111b033c4bb13eb174b19e662
Hank <aken1215@hotmail.com>
2017-02-12 13:51:30 +0800

    第6-1步驟 將hero component 從app component拆出來到獨自的component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeroComponentComponent } from './hero-component/hero-component.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -9,6 +10,7 @@ import { AppComponent }  from './app.component';
     FormsModule
   ],
   declarations: [
+    HeroComponentComponent,
     HeroDetailComponent,
     AppComponent
   ],
